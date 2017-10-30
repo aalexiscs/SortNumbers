c2783b2441c298668d194fa9bcca2de7ee1e2727
Alexis Contreras <aalexiscs@outlook.com>
2017-10-30 10:02:19 -0700

    I upload the solution

---
+++ SortNumbers/SortNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of values want to enters");
+            int NumberValues = int.Parse(Console.ReadLine());
+            int[] Numbers = new int[NumberValues];
+
+            for (int i = 0; i < NumberValues; i++)
+            {
+                Console.WriteLine(string.Format("Enter the value: {0}, please", i + 1));
+                Numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(Numbers);
+            Console.WriteLine("The values are: " + string.Join(",", Numbers));
+            Console.ReadKey();
+        }
+    }
+}
